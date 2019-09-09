e9f0a6437fb41bf251bbe391b62f115e0b42ad12
david511382 <david511382@yahoo.com.tw>
2019-09-09 15:02:29 +0800

    加入gateway

---
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace OcelotApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                .ConfigureAppConfiguration(
+                    (hostContext, config) =>
+                    {
+                        IHostingEnvironment env = hostContext.HostingEnvironment;
+                        config.SetBasePath(Path.Combine(env.ContentRootPath, "."))
+                            .AddJsonFile(path: "configuration.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile(path: $"configuration.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    })
+                .UseStartup<Startup>();
+
+            return builder;
+        }
+    }
+}
