8ff630e8bd23ea218dff9375851ea80ed893092d
david511382 <twdavid@dafacloud.com>
2020-01-08 15:16:12 +0800

    Game api接上ES 修正Lobby log name

--- src/Services/Game/BoardGameWebService/Program.cs
+++ src/Services/Game/BoardGameWebService/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System.IO;
 
 namespace BoardGameWebService
 {
@@ -14,11 +11,26 @@ namespace BoardGameWebService
     {
         public static void Main(string[] args)
         {
+            NLogBuilder.ConfigureNLog("NLog.config");
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseNLog()
+                .UseStartup<Startup>()
+                .ConfigureLogging((hostContext, logging) =>
+                {
+                    IHostingEnvironment env = hostContext.HostingEnvironment;
+                    string configFileName = (env.EnvironmentName.Equals("Release")) ?
+                    $"appsettings.json" :
+                    $"appsettings.{env.EnvironmentName}.json";
+                    IConfigurationRoot configuration = new ConfigurationBuilder()
+                        .SetBasePath(Path.Combine(env.ContentRootPath, "."))
+                        .AddJsonFile(path: configFileName, optional: true, reloadOnChange: true)
+                        .Build();
+                    logging.ClearProviders();
+                    logging.AddConfiguration(configuration.GetSection("Logging"));
+                });
     }
 }
--- src/Services/Game/BoardGameWebService/Startup.cs
+++ src/Services/Game/BoardGameWebService/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Domain.Logger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using NLog.Extensions.Logging;
 
 namespace BoardGameWebService
 {
@@ -28,12 +25,16 @@ namespace BoardGameWebService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddNLog();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMiddleware<HttpLoggerMiddleware>();
 
             app.UseMvc();
         }
