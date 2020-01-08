f4287daa2f2d9ca06d060ad74382f7da1e1c7fa3
david511382 <twdavid@dafacloud.com>
2020-01-08 15:55:05 +0800

    Game api 使用Swagger

--- src/Services/Game/GameWebService/Startup.cs
+++ src/Services/Game/GameWebService/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Logger;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace GameWebService
 {
@@ -21,7 +26,47 @@ namespace GameWebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    // name: 攸關 SwaggerDocument 的 URL 位置。
+                    name: "development",
+                    // info: 是用於 SwaggerDocument 版本資訊的顯示(內容非必填)。
+                    info: new Info
+                    {
+                        Title = "Game API",
+                        Version = "1.0.0",
+                        Description = "認證與授權",
+                        TermsOfService = "https://github.com/david511382/BoardGame",
+                        Contact = new Contact
+                        {
+                            Name = "David",
+                            Url = "https://github.com/david511382/BoardGame"
+                        }
+                    }
+                );
+
+                c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+                    new ApiKeyScheme
+                    {
+                        In = "header",
+                        Description = "在JWT前面加上User Json",
+                        Name = "Authorization",
+                        Type = "apiKey"
+                    });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                    {
+                        JwtBearerDefaults.AuthenticationScheme,
+                        new string[]{ }
+                    },
+                });
+
+                string xmlFile = "Api.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +80,17 @@ namespace GameWebService
             }
 
             app.UseMiddleware<HttpLoggerMiddleware>();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    // url: 需配合 SwaggerDoc 的 name。 "/swagger/{SwaggerDoc name}/swagger.json"
+                    url: "/swagger/development/swagger.json",
+                    // name: 用於 Swagger UI 右上角選擇不同版本的 SwaggerDocument 顯示名稱使用。
+                    name: "development"
+                );
+            });
 
             app.UseMvc();
         }
