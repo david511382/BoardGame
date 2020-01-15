09ff907fb2a096d5f6dfc7571bdd18f75b61dc83
david511382 <twdavid@dafacloud.com>
2020-01-15 11:15:38 +0800

    修正 前端bigtwo檔名

---
+++ src/WebUI/BoardGameAngular/ClientApp/src/app/game/bigtwo/bigtwo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-bigtwo',
+  templateUrl: './bigtwo.component.html',
+  styleUrls: ['./bigtwo.component.css']
+})
+export class BigtwoComponent{
+  constructor() {}
+}
